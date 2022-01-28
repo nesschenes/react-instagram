97695a9512b8ee8da40a1c2822d13e121ccf649c
Ness <nesschen1101@gmail.com>
2022-01-28 18:18:41 +0800

    set up basic project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import LOGO from "../asset/logo.png";
+
+export const App = () => {
+  return (
+    <>
+      <h1>
+        {process.env.NODE_ENV} - {process.env.name}
+      </h1>
+      <img src={LOGO} alt="Logo" />
+    </>
+  );
+};
