aacd9af5c9cd2eee6c57b8337b8bf5fffb5df042
Ness Chen <nesschen1101@gmail.com>
2022-02-01 17:03:22 +0800

    fix husky and refactor webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,9 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
 
 module.exports = (envs) => {
-  const { env } = envs;
-  const patch = require(`./webpack.${env}.js`);
-  const config = merge(common, patch);
-  return config;
-};
+  const { env } = envs
+  const patch = require(`./webpack.${env}.js`)
+  const config = merge(common, patch)
+  return config
+}
