f63cef911133bc33007f2b51771d06da2a85799c
Ness Chen <nesschen1101@gmail.com>
2022-02-09 16:46:33 +0800

    use custom firestore functions instead of react-firebase-hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
 import './App.css'
 import NAV from '../asset/nav-instagram.png'
-import { Post /*, Props as PostData*/ } from './Post'
+import { Post, Props as PostData } from './Post'
 // import { useState, useEffect } from 'react'
 // import { v4 } from 'uuid'
-import { useCollection } from 'react-firebase-hooks/firestore'
-import { collection } from 'firebase/firestore'
-import { db } from './firebase'
+import { tryGetDocs } from './firebase'
+import { useState } from 'react'
 
 export const App = () => {
-  // const [posts] = useState<PostData[]>([
-  //   {
-  //     key: v4(),
-  //     iconUrl: '/static/images/avatar/1.jpg',
-  //     username: 'Ness',
-  //     caption: 'Hello! Everyone!',
-  //     imageUrl: 'https://picsum.photos/800/400?random=1',
-  //   },
-  //   {
-  //     key: v4(),
-  //     iconUrl: '/static/images/avatar/2.jpg',
-  //     username: 'Jill',
-  //     caption: 'Hello! Everyone!',
-  //     imageUrl: 'https://picsum.photos/800/400?random=2',
-  //   },
-  // ])
-
-  const [value] = useCollection(collection(db, 'posts'), {
-    snapshotListenOptions: { includeMetadataChanges: true },
-  })
+  const [posts, setPosts] = useState<PostData[]>([])
+  tryGetDocs<PostData>('posts').then((p) => setPosts(p))
 
   return (
     <>
@@ -35,13 +16,13 @@ export const App = () => {
         <img className="app--nav" src={NAV} alt="nav-logo" />
       </div>
       {/*header*/}
-      {value?.docs.map((post) => (
+      {posts.map((post) => (
         <Post
-          key={post.id}
-          iconUrl={post.data().iconUrl}
-          username={post.data().username}
-          caption={post.data().caption}
-          imageUrl={post.data().imageUrl}
+          key={post.key}
+          iconUrl={post.iconUrl}
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
         />
       ))}
     </>
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  setDoc,
+  doc,
+  getDoc,
+  query,
+  QueryConstraint,
+} from 'firebase/firestore'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyCUdul7xBXB2NSOErjPh9YEroKobNpe9Bg',
   authDomain: 'react-instagram-41e34.firebaseapp.com',
@@ -17,8 +23,72 @@ const firebaseConfig = {
   appId: '1:256847975659:web:de48eb0300a9d7b4b5daae',
 }
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
 
-export { app, db }
+export const tryAddDoc = async (collectionName: string, data: object) => {
+  try {
+    const docRef = await addDoc(collection(db, collectionName), data)
+    console.log('Document written with ID: ', docRef.id)
+  } catch (e) {
+    console.error('Error adding document: ', `${collectionName} `, e)
+  }
+}
+
+export const trySetDoc = async (
+  collectionName: string,
+  docName: string,
+  data: object
+) => {
+  try {
+    await setDoc(doc(db, collectionName, docName), data)
+    console.log('Document written with ID: ', docName)
+  } catch (e) {
+    console.error(
+      'Error setting document: ',
+      `${collectionName}/${docName} `,
+      e
+    )
+  }
+}
+
+export const tryGetDoc = async <T>(
+  collectionName: string,
+  docName: string
+): Promise<T | null> => {
+  try {
+    const snap = await getDoc(doc(db, collectionName, docName))
+    if (!snap.exists()) throw `No such document!`
+    console.log('Document data: ', snap.data())
+    return snap.data() as T
+  } catch (e) {
+    console.error(
+      'Error getting document: ',
+      `${collectionName}/${docName} `,
+      e
+    )
+    return null
+  }
+}
+
+export const tryGetDocs = async <T>(
+  collectionName: string,
+  queryConstraint: QueryConstraint | null = null
+): Promise<T[]> => {
+  try {
+    let snap: T[]
+    if (queryConstraint) {
+      const q = query(collection(db, collectionName), queryConstraint)
+      snap = (await getDocs(q).then((s) => s.docs)).map((d) => d.data() as T)
+    } else {
+      snap = (
+        await getDocs(collection(db, collectionName)).then((s) => s.docs)
+      ).map((d) => d.data() as T)
+    }
+    console.log('Documents data: ', snap)
+    return snap
+  } catch (e) {
+    console.error('Error getting documents: ', `${collectionName} `, e)
+    return []
+  }
+}
